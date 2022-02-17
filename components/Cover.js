f9d18c9b1d3f99da35a8e30e640dd31212390750
AleGomez15213 <alegomez15213@gmail.com>
2022-02-16 22:24:48 -0800

    Added cover images (work in progress)

--- components/Cover.js
+++ components/Cover.js
@@ -2,13 +2,20 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Cover({ info }) {
+    const API_URL = process.env.API_URL;
     const {name, id, cover} = info;
+    /* Set up .env.local in project folder with this info:
+    
+    API_URL=http://localhost:1337/
+    IMAGES_DOMAIN=localhost
+    
+    */
     return (
         <div>
             <div>
                 <a>{name}</a>
-                    <Image
-                        src={cover.url}
+                    <Image // API_URL not working when passed as prop but regular string works fine
+                        src={API_URL + "uploads/batman_year_One_6fc218bc44.jpg"}
                         alt={name + " cover"}
                         width={500}
                         height={500}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  
+  env: {
+    API_URL: process.env.API_URL,
+    IMAGES_DOMAIN: process.env.IMAGES_DOMAIN
+  },
+
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    IMAGES_DOMAIN: process.env.IMAGES_DOMAIN
+  },
+
+  images: {
+    domains: [process.env.IMAGES_DOMAIN],
+    path: '/_next/image',
+    loader: 'default',
+  },
 }
